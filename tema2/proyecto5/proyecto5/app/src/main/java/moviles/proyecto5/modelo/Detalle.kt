a8dc6e63a573b01971aaaf7281b9d008d43364c4
cristdc <cristinadiazcabello@gmail.com>
2024-12-12 12:25:24 +0100

    actualizado

--- tema2/proyecto5/proyecto5/app/src/main/java/moviles/proyecto5/modelo/Detalle.kt
+++
@@ -1,9 +0,0 @@
-package moviles.proyecto5.modelo
-
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class Detalle(
-    val color:String,
-    val tallas:List<String>
-)
